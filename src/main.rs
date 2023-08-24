d65afd6b44ffe64d5d694f4f3c57f2756b3672d6
Chris Combs <combscp@gmail.com>
2023-08-23 22:20:29 -0400

    Chapter 3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use z2p::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
